53da03fde4d8890e5a3570353fa4b74b21a935eb
Erik Zbik <erik.zbik@gmail.com>
2019-07-04 00:19:28 +0200

    w

---
+++ watson/watson_log.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# designed to accept `watson log` on stdin and make more cool
+
+import sys, json;
+import requests
+
+from datetime import datetime, timedelta
+
+from dateutil.parser import parse
+
+data = json.load(sys.stdin)
+
+
+total_time_spent=timedelta()
+
+for item in data:
+    #print (item,"\n")
+
+    date1 = parse(item['start'])
+    date2 = parse(item['stop'])
+    time_spent=date2 - date1
+    total_time_spent+=time_spent
+    id=item['id'][0:8]
+    line=date1.strftime("%F") + ", from "   \
+        + date1.strftime("%H:%M") + " to " + date2.strftime("%H:%M") + \
+        ", spent " + str(time_spent)+" on [ " + id + " @ "+ ",".join(item['tags'] ) + "]"
+    print (line)
+
+print ("TOTAL "+str(total_time_spent))
