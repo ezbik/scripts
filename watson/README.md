8e229e8967d9ed568135ce0da5df8ed9586355d0
Pavel Piatruk <erik.zbik@gmail.com>
2020-09-08 02:37:10 +0300

    +customer field

---
+++ watson/README.md
@@ -0,0 +1,38 @@
+
+## Watson log wrapper
+
+
+This wrapper is designed to accept `watson log` on stdin and make it more cool & detailed:
+
+Sample usage:
+           `~/bin/watson log -j  | python3 ~/bin/watson_log.py`
+
+   where
+  ` ~/bin/watson`            orig watson script
+  ` ~/bin/watson_log.py`     this script.
+
+   Output:
+
+```
+ 2020-09-03, from 15:40 to 16:40, spent 1:00:00 for customer : XXX, on [cde73c79] upgrade server
+ 2020-09-05, from 01:40 to 03:10, spent 1:30:00 for customer : YYY, on [9ec97d69] mail sync
+ 2020-09-05, from 23:22 to 23:52, spent 0:30:00 for customer : YYY, on [57cc6dba] sync to gmail
+ TOTAL 3:00:00
+```
+
+ You can also create a bash function to overwrite `watson log`
+
+```
+watson () 
+{ 
+    case $1 in 
+        log)
+            ~/bin/watson $@ -j | python3 ~/bin/watson_log.py
+        ;;
+        *)
+            ~/bin/watson $@
+        ;;
+    esac
+}
+```
+
--- watson/watson_log.py
+++ watson/watson_log.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python3
 
-# designed to accept `watson log` on stdin and make more cool
-# then create a bash function to overwrite `watson log`
-
-#watson () 
-#{ 
-#    case $1 in 
-#        log)
-#            ~/myenv/bin/watson $@ -j | python3 ~/bin/watson_log.py
-#        ;;
-#        *)
-#            ~/myenv/bin/watson $@
-#        ;;
-#    esac
-#}
-
+# designed to accept `watson log` on stdin and make it more cool & detailed.
 
 import sys, json;
 import requests
@@ -31,6 +17,7 @@ total_time_spent=timedelta()
 for item in data:
     #print (item,"\n")
 
+    project = item['project']
     date1 = parse(item['start'])
     date2 = parse(item['stop'])
     time_spent=date2 - date1
@@ -38,7 +25,8 @@ for item in data:
     id=item['id'][0:8]
     line=date1.strftime("%F") + ", from "   \
         + date1.strftime("%H:%M") + " to " + date2.strftime("%H:%M") + \
-        ", spent " + str(time_spent)+" on [ " + id + " @ "+ ",".join(item['tags'] ) + "]"
+        ", spent " + str(time_spent)+" for customer : " + project + ", on [" + id + "] " +  ",".join(item['tags'] ) 
     print (line)
 
 print ("TOTAL "+str(total_time_spent))
+
